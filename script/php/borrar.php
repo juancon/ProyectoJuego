2e03615827c46449deee5681c79930718b701d10
juancon <33897333+juancon@users.noreply.github.com>
2019-02-20 17:29:18 +0100

    borrado

--- script/php/borrar.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once 'ConexionBD.php';
-	$conexion = ConexionDB::connectDB();
-	$insertarConsulta = "DELETE FROM usuarios where puntuacion < 8500";
-	$insertar = $conexion->exec($insertarConsulta);
- ?>
